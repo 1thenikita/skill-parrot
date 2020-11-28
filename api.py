d7fca91378093a337994ee23b68771108ee13eee
BuildTools <unconfigured@null.spigotmc.org>
2020-11-28 21:39:28 +0300

    Init parrot

---
+++ api.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+# Импортирует поддержку UTF-8.
+from __future__ import unicode_literals
+
+# Импортируем модули для работы с JSON и логами.
+import json
+import logging
+
+# Импортируем подмодули Flask для запуска веб-сервиса.
+from flask import Flask, request
+app = Flask(__name__)
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Хранилище данных о сессиях.
+sessionStorage = {}
+
+# Задаем параметры приложения Flask.
+@app.route("/", methods=['POST'])
+
+def main():
+# Функция получает тело запроса и возвращает ответ.
+    logging.info('Request: %r', request.json)
+
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    handle_dialog(request.json, response)
+
+    logging.info('Response: %r', response)
+
+    return json.dumps(
+        response,
+        ensure_ascii=False,
+        indent=2
+    )
+
+# Функция для непосредственной обработки диалога.
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        # Это новый пользователь.
+        # Инициализируем сессию и поприветствуем его.
+
+        res['response']['text'] = 'Скажите что-нибудь, чтобы наш попугай повторил это.'
+        return
+
+    # Если нет, то убеждаем его купить слона!
+    res['response']['text'] = '%s' % (
+        req['request']['original_utterance']
+    )
